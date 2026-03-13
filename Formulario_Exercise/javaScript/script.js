f20acf1a4a2b4ec9fb3414b9f92a8a14ee1fc87e
Yilmerher12_ <yilmerpdf@gmail.com>
2026-03-13 12:46:02 -0500

    FEAT: improve the proyect by adding more impputs, validations and structures

--- Formulario_Exercise/javaScript/script.js
+++ Formulario_Exercise/javaScript/script.js
@@ -1,20 +1,62 @@
-const btnEnviar = document.getElementById("btnEnviar");
+let correctPassword = "ADSO2026";
+
+const btnEnviar = document.querySelector("#btnIngresar");
 const inputAge = document.getElementById("userAge");
-const text = document.getElementById("changeText");
+const text = document.getElementById("textMesage");
+const inputName = document.getElementById("userName");
+const password= document.querySelector("#passwordAccount");
 
 btnEnviar.addEventListener("click", function(){
-    
-    if(Number(inputAge.value) >= 18){
-        inputAge.classList.remove("borde-rojo"); //limpiar
-        inputAge.classList.add("borde-verde")
-
-        text.textContent = `Tu puedes entrar a la discoteca`
-        text.classList.remove("error");
-    }else{
-        inputAge.classList.remove("borde-verde"); 
-        inputAge.classList.add("borde-rojo")
-        text.classList.add("error")
-        text.textContent= `Error: Estas ingresando una edad no valida`
+    //Validaciones
+    if(inputName.value === ""){
+        inputName.classList.add("error-input");
+        
+        text.classList.add("alert-text");
+        text.textContent = `Debes escribir un nombre valido`
+        
+        return;
     }
+
+    inputName.classList.remove("error-input");
+    text.classList.remove("alert-text")
+
+
+    if(Number(inputAge.value) <= 17){
+        inputAge.classList.add("error-input");
+        
+        text.classList.add("alert-text");
+        text.textContent = `ERROR: No se permite menores de edad`
+    
+        return;
+    }
+    inputAge.classList.remove("error-input");
+    text.classList.remove("alert-text")
+
+    if(password.value != correctPassword){
+        password.classList.add("error-input");
+        
+        text.classList.add("alert-text");
+        text.textContent = `ERROR: La contrañsea no es la correcta`
+        return;
+    }
+
+    password.classList.remove("error-input");
+    text.classList.remove("alert-text")
+
+
+    inputName.classList.add("input-exito");
+    inputAge.classList.add("input-exito");
+    password.classList.add("input-exito");
+
+    // 2. Cambiamos el H1 (necesitas capturarlo arriba primero con un id)
+    const titulo = document.querySelector("h1"); 
+    titulo.textContent = `Agente ${inputName.value} Activado`;
+
+    // 3. Deshabilitamos el botón para que no sigan dando clic
+    btnEnviar.disabled = true;
+
+    // 4. Mensaje final de éxito
+    text.classList.remove("alert-text");
+    text.textContent = "Acceso total concedido";
 
 })
